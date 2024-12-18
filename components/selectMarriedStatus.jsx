7b1d0769a16a7f57378c8b8cd787a5423c398fd4
Arif Billah <arifbillah0156@gmail.com>
2024-12-18 18:14:49 +0600

    Update selectMarriedStatus.jsx

--- components/selectMarriedStatus.jsx
+++ components/selectMarriedStatus.jsx
@@ -6,7 +6,7 @@ export default function SelectMarriedStatus({ all }) {
       {all ? <option>সকল</option> : ""}
       <option value="অবিবাহিত">অবিবাহিত</option>
       <option value="ডিভোর্সড">ডিভোর্সড</option>
-      <option value="বিধবা">বিধবা</option>
+      <option value="বিধবা/বিপত্নীক">বিধবা/বিপত্নীক</option>
     </>
   );
 }
