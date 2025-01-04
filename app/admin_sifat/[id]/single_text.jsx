9803fe296f3efa69c3291daab6324f0e5bd4663f
Arif Billah <arifbillah0156@gmail.com>
2025-01-04 20:48:13 +0600

    Update single_text.jsx

--- app/admin_sifat/[id]/single_text.jsx
+++ app/admin_sifat/[id]/single_text.jsx
@@ -1,7 +1,7 @@
 export default function Text({ text, value }) {
   return (
     <div>
-      <span className="font-semibold text-gray-800 text-xl">{text}:</span>
+      <span className="font-semibold text-gray-800 text-xl">{text}: </span>
       <span className="text-gray-700 text-lg">{value}</span>
     </div>
   );
